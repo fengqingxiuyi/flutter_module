3bd04269a34ed1c7f03d6fe6bf087af9bf499b77
fqxyi <fengqingxiuyi@gmail.com>
2020-09-02 19:07:56 +0800

    微小的改动

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'FlutterModule',
+      title: 'flutter_module',
       builder: FlutterBoost.init(postPush: _onRoutePushed),
       theme: ThemeData(
           scaffoldBackgroundColor: ColorConstant.backColor,
