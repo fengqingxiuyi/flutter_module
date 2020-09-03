5473fcc4a7b43394d144e01f1435ac37fe166bf9
fqxyi <fengqingxiuyi@gmail.com>
2020-09-03 17:04:02 +0800

    统一flutter页面跳转方式 & 解决release包运行错误的问题

--- lib/constants/routes.dart
+++ lib/constants/routes.dart
@@ -3,8 +3,11 @@
 class RouteConstant {
 
   /// Flutter页面
+  static const String flutterScheme = "flutter://";
   //测试界面
-  static const String flutterTest = "flutter://test";
+  static const String flutterTest = flutterScheme + "test";
+  //ECharts页面
+  static const String flutterECharts = flutterScheme + "echarts";
 
   /// Native页面
   static const String nativeScheme = "flutterandroid://";
